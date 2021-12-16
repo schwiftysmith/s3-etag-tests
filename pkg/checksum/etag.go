e4bb970f92c2aa563def48bb92646b67697267e6
Oliver Lutz <oliver.lutz@siemens.com>
2021-12-16 18:53:37 +0100

    Adjusted confusing code

--- pkg/checksum/etag.go
+++ pkg/checksum/etag.go
@@ -6,14 +6,12 @@ import (
 	"github.com/peak/s3hash"
 )
 
-func CalculateChecksum(path string, chunkSizeInMB int64) (string, error) {
+func CalculateChecksum(path string, chunkSize int64) (string, error) {
 	f, err := os.Open(path)
 	if err != nil {
 		return "", err
 	}
 	defer f.Close()
-
-	chunkSize := chunkSizeInMB
 
 	return s3hash.Calculate(f, chunkSize)
 }
